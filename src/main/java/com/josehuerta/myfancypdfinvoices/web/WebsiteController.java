f65f7202f8fb972324d0f13bb77370712c2776d5
Jose Huerta <huertavj@oregonstate.edu>
2024-04-10 11:58:01 -0700

    Add form bean, and GET and POST methods for handling form submissions - with validation.

--- src/main/java/com/josehuerta/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/josehuerta/myfancypdfinvoices/web/WebsiteController.java
@@ -1,8 +1,13 @@
 package com.josehuerta.myfancypdfinvoices.web;
 
+import com.josehuerta.myfancypdfinvoices.web.forms.LoginForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
@@ -19,5 +24,30 @@ public class WebsiteController {
         model.addAttribute("username", username);
         model.addAttribute("currentDate", LocalDateTime.now());
         return "index.html";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
+        return "login.html";
+    }
+
+    @PostMapping("/login")
+    /*
+    The @ModelAttribute annotation makes sure that you can access the loginForm in the model
+    without having to manually add it.
+    */
+    public String login(@ModelAttribute @Valid LoginForm loginForm, BindingResult bindingResult, Model model) {
+        /*
+        The BindingResult parameter is basically a container for our validation errors.
+        */
+        if(bindingResult.hasErrors()) {
+            return "login.html";
+        }
+        if(loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        model.addAttribute("invalidCredentials", "true");
+        return "login.html";
     }
 }
