4d7e22f76e14777211ff66a6c2cc5963a46cc3a5
Jose Huerta <huertavj@oregonstate.edu>
2024-04-10 11:33:55 -0700

    Add variables to Thymeleaf template.
    Also add a model to send a username and current date values to template.

--- src/main/java/com/josehuerta/myfancypdfinvoices/web/WebsiteController.java
+++ src/main/java/com/josehuerta/myfancypdfinvoices/web/WebsiteController.java
@@ -1,13 +1,23 @@
 package com.josehuerta.myfancypdfinvoices.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class WebsiteController {
 
     @GetMapping("/")
-    public String homepage() {
+    /*
+    Here, the model parameter is essentially a map containing all the variables we want to use in our Thymeleaf
+    templates.
+    */
+    public String homepage(Model model, @RequestParam(name = "username", required = false, defaultValue = "stranger") String username) {
+        model.addAttribute("username", username);
+        model.addAttribute("currentDate", LocalDateTime.now());
         return "index.html";
     }
 }
